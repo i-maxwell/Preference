ef9d77625f5b13f2e4f54538e0d942d4dd672202
murphyyang <muephyyang@tencent.com>
2019-01-08 21:44:41 +0800

    提交Alfred3配置文件

---
+++ workflows/user.workflow.CCD01991-3517-4A46-8320-0D7BE24C451B/gz_mnet.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = $argv[1];
+
+echo '<items>
+    <item autocomplete = "autocompletex" uid = "123321" arg = "argsx" >
+    <title >' . $a . '</title>
+    <subtitle >subtitle</subtitle>
+    <icon >icon</icon>
+    </item>
+</items>';
+
+
